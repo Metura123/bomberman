1fef96d1b8f8368648dba714caadff93b3cef7c1
Metudu <tarikucar@gmail.com>
2023-05-05 20:43:57 +0300

    Functions has updated

--- src/utils.py
+++ src/utils.py
@@ -5,22 +5,20 @@ BOMB = pygame.image.load("img/bomb-32.png")
 BARREL = pygame.image.load("img/barrel-32.png")
 # End Images
 
-def draw_pixels(WINDOW,file_location):
+def draw_pixels(WINDOW,minimap):
     x = 0
     y = 0
     resolution = 32
-
-    with open(file_location, "r") as file:
-        for line in file:
-            for id in line:
-                if id == "1":
-                    WINDOW.blit(STONE,(x, y))
-                if id == "2":
-                    WINDOW.blit(BOMB, (x, y))
-                if id == "3":
-                    WINDOW.blit(BARREL, (x, y))
-                x += resolution
+    for row in minimap:
+        for column in row:
+            if column == 1:
+                WINDOW.blit(STONE,(x, y))
+            if column == 2:
+                WINDOW.blit(BOMB, (x, y))
+            if column == 3:
+                WINDOW.blit(BARREL, (x, y))
+            x += resolution
             
-            x = 0
-            y += resolution
+        x = 0
+        y += resolution
 
