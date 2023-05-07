19d06132dfe728717dda3d56e5b4dab8679e7785
Metudu <tarikucar@gmail.com>
2023-05-07 22:05:46 +0300

    Server has implemented

--- src/test.py
+++
@@ -1,8 +0,0 @@
-import socket, threading, pickle
-
-IP_ADDRESS = "localhost"
-PORT = 9999
-
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((IP_ADDRESS, PORT))
-print("Connected!")
